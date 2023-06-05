dfd00a497cc0046c3d6ebd55702e68372476519f
NhanHV-Rikai <nhan.ho@rikai.technology>
2023-06-05 13:55:00 +0700

    Update notify

--- service-worker.js
+++ service-worker.js
@@ -8,6 +8,17 @@ const options = {
   iconUrl: "assets/icons/icon-128.png",
 };
 
+try {
+  STORAGE.local.get(["notifications"], (data) => {
+    notificationsInit = data.notifications
+      ? JSON.parse(data.notifications)
+      : [];
+    notifyPermission();
+  });
+} catch (error) {
+  logError(error);
+}
+
 const notify = (message, notification) => {
   options.message = message;
   chrome.notifications.create('', options, () => {
@@ -29,7 +40,7 @@ const getDelayInMinutes = (timeDate) => {
   return (timeInMilliseconds - currentTime) / (1000 * 60);
 }
 
-const deleteAllAlarm = () => {
+const clearAllAlarm = () => {
   ALARMS.clearAll(function(wasCleared) {
     if (wasCleared) {
       logError("Successfully cleared all alarms");
@@ -39,38 +50,40 @@ const deleteAllAlarm = () => {
   });
 }
 
-try {
-  STORAGE.local.get(["notifications"], (data) => {
-    notificationsInit = data.notifications
-      ? JSON.parse(data.notifications)
-      : [];
-  });
-} catch (error) {
-  logError(error);
+const createNotifyAlarms = (notificationsInit) => {
+  if (notificationsInit.length > 0) {
+    notificationsInit.forEach((notification) => {
+      let timeDate = notification.time;
+      let message = notification.message;
+
+      let timeInMinutes = getDelayInMinutes(timeDate)
+    
+      if (!isNaN(timeInMinutes)) {
+        ALARMS.create('notificationAlarm' + notification.id, { delayInMinutes: timeInMinutes });
+      }
+    
+      ALARMS.onAlarm.addListener((alarm) => {
+          if (alarm.name === 'notificationAlarm' + notification.id) {
+          notify(message, notification);
+        }
+      });
+    });
+  }
+}
+
+const notifyPermission = () => {
+  if (typeof chrome !== 'undefined' && typeof chrome.notifications !== 'undefined') {
+    createNotifyAlarms(notificationsInit);
+  } else {
+    logError("Browser doesn't support Notification API.");
+  }
 }
 
 STORAGE.onChanged.addListener((changes, namespace) => {
   if (changes.notifications) {
     let data = changes?.notifications.newValue;
     notificationsInit = data ? JSON.parse(data) : [];
-    deleteAllAlarm();
-    if (notificationsInit.length > 0) {
-      notificationsInit.forEach((notification) => {
-        let timeDate = notification.time;
-        let message = notification.message;
-
-        let timeInMinutes = getDelayInMinutes(timeDate)
-      
-        if (!isNaN(timeInMinutes)) {
-          ALARMS.create('notificationAlarm' + notification.id, { delayInMinutes: timeInMinutes });
-        }
-      
-        ALARMS.onAlarm.addListener((alarm) => {
-          if (alarm.name === 'notificationAlarm' + notification.id) {
-            notify(message, notification);
-          }
-        });
-      });
-    }
+    clearAllAlarm();
+    createNotifyAlarms(notificationsInit);
   }
 });
